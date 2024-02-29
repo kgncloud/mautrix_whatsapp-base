da6d74146ec1eb226d18d15a04df7c3b59cca995
Andrew Ferrazzutti <andrewf@element.io>
2024-02-29 16:10:14 -0500

    Add config to disallow manual double puppeting

--- config/upgrade.go
+++ config/upgrade.go
@@ -76,6 +76,7 @@ func DoUpgrade(helper *up.Helper) {
 	} else {
 		helper.Copy(up.Map, "bridge", "login_shared_secret_map")
 	}
+	helper.Copy(up.Bool, "bridge", "allow_manual_double_puppeting")
 	if legacyPrivateChatPortalMeta, ok := helper.Get(up.Bool, "bridge", "private_chat_portal_meta"); ok {
 		updatedPrivateChatPortalMeta := "default"
 		if legacyPrivateChatPortalMeta == "true" {
